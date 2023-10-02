c86f0e9a9ad2b10834cae77ddd633102c4539740
Fuad Abou Heit <f.abouheit@gmail.com>
2023-10-03 00:07:42 +0300

    set up mongoose and created database

--- api/index.js
+++ api/index.js
@@ -1,12 +1,25 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const PORT = 3500;
-const app = express();
-
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`)
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected to MongoDB!")
+}).catch((err)=>{
+    console.error('Error connecting to mongoDB', err);  
 })
 
+const PORT = 3504;
+const app = express();
+
+try{
+app.listen(PORT, () => { 
+    console.log(`Server is running on port ${PORT}`)
+    })
+} catch { 
+    console.error('Error starting server')
+}
+
 app.get("/",(req,res)=>{
-    res.send({message: "hello"})
+    res.send({message: "helddlo"})
 })
